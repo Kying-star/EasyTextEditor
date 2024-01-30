826a4901301d0fff9af6c43088b874dfef10e74a
Kying-star <3077006505@qq.com>
2024-01-30 22:34:09 +0800

    feat

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,4 @@
+use crate::Position;
 use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
@@ -29,9 +30,14 @@ impl Terminal {
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
-    pub fn cursor_position(x: u16, y: u16) {
-        let x = x.saturating_add(1);
-        let y = y.saturating_add(1);
+
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cursor_position(position: &Position) {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
         print!("{}", termion::cursor::Goto(x, y));
     }
     pub fn flush() -> Result<(), std::io::Error> {
@@ -43,5 +49,14 @@ impl Terminal {
                 return key;
             }
         }
+    }
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
     }
 }
